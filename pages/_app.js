ade9a1639e76b35a7285a870a11a3e1fa0cf8cb6
Zohid <zohid.valiev@gmail.com>
2022-01-28 20:19:10 +0500

    Удален import loading-bar.css из _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import '@/styles/globals.css'
 import '@/styles/block.css'
 import '@/styles/form.css'
 import '@/styles/icon.css'
-import '@/styles/loading-bar.css'
 import '@/styles/loading.css'
 import { ConfirmDialog } from "@/components/confirm-dialog"
 import { Message } from '@/components/message'
