949cf8b9e383e24c015dcba09516e599b5aa4ab1
Zohid Valiev <zohid.valiev@gmail.com>
2022-02-25 20:13:22 +0500

    Удален неиспользуемый компонент и index.js переведен на ts

--- components/users/froms/index.js
+++
@@ -1,6 +0,0 @@
-
-export { default as ChangeNicknameForm } from "./change-nickname/FormContainer"
-export { default as ChangeRoleForm } from "./change-role/FormContainer"
-export { default as ChangeStatusForm } from "./change-status/FormContainer"
-export { default as CreateForm } from "./create/FormContainer"
-// export { default as FormValue } from "./form-value/Form"
---
+++ components/users/froms/index.ts
@@ -0,0 +1,5 @@
+
+export { default as ChangeNicknameForm } from "./change-nickname/FormContainer"
+export { default as ChangeRoleForm } from "./change-role/FormContainer"
+export { default as ChangeStatusForm } from "./change-status/FormContainer"
+export { default as CreateForm } from "./create/FormContainer"
