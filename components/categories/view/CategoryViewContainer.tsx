14e013762cace33f78878618ce3b94f9308018b7
Zohid Valiev <zohid.valiev@gmail.com>
2022-05-08 20:17:34 +0500

    Добавлен tool-bar на странице просмотра категории

--- components/categories/view/CategoryViewContainer.tsx
+++ components/categories/view/CategoryViewContainer.tsx
@@ -5,20 +5,34 @@ import { FC, useEffect } from 'react'
 import CategoryView from "./CategoryView"
 import { ViewGridSpinner } from "@/components/common/spinners/view-grid"
 import { _infoLoader } from '@/components/common/loaders/info-loader'
+import { CategoryViewToolBar } from "@/components/categories/view-tool-bar"
+import events from "@/events-bus"
 
 const CategoryViewContainer: FC = () => {
 
-    const { query } = useRouter()
+    const { query, push } = useRouter()
     const id = parseInt((query.id as unknown) as string)
-    const { data: category, isLoading } = useCategory(id)
+    const { data: category, isLoading, isValidating, mutate } = useCategory(id)
 
     useEffect(() => {
-        if (isLoading) {
+        return events.categoryUpdated.subscribe((id) => {
+            mutate()
+        })
+    }, [mutate])
+
+    useEffect(() => {
+        return events.categoryDeleted.subscribe((id) => {
+            push('/categories')
+        })
+    }, [])
+
+    useEffect(() => {
+        if (isLoading || isValidating) {
             _infoLoader.open()
         } else {
             _infoLoader.close()
         }
-    }, [isLoading])
+    }, [isLoading, isValidating])
 
     if (isLoading) {
         return (
@@ -27,7 +41,10 @@ const CategoryViewContainer: FC = () => {
     }
 
     return (
-        <CategoryView category={category}/>
+        <>
+            <CategoryViewToolBar category={category}/>
+            <CategoryView category={category}/>
+        </>
     )
 }
 
